0a927015033a59957d664a2813fc641f12ef1297
trumanfromkorea <jangjh971008@naver.com>
2022-10-23 00:03:17 +0900

    swiftlint.yml 생성 후 적용

--- MyApp/MyAppTests/MyAppTests.swift
+++ MyApp/MyAppTests/MyAppTests.swift
@@ -5,11 +5,10 @@
 //  Created by 장재훈 on 2022/10/22.
 //
 
-import XCTest
 @testable import MyApp
+import XCTest
 
 final class MyAppTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,9 +27,8 @@ final class MyAppTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
